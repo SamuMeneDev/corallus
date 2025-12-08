0cb899512003702bd26720aa962e7f3959c50478
Samu Mene <samuelsantiago2222@gmail.com>
2025-12-08 18:42:40 -0300

    Empresas Parceiras

--- email.php
+++ email.php
@@ -1,5 +1,13 @@
 <?php
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $assunto = $_POST['assunto'];
+    $dados = [
+        $nome = $_POST['nome'],
+        $email = $_POST['email'],
+        $assunto = $_POST['assunto']
+    ];
+    foreach ($dados as $dado) {
+        if(!isset($dado)) {
+            die("Erro: Dados não preenchidos");
+        }
+    }
+    
 ?>
